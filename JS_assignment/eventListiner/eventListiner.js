b62ddcb11243095a3e296760807e36db2d2b44a1
HasemMallick <101096018+HasemMallick@users.noreply.github.com>
2023-04-09 09:51:45 +0530

    Add files via upload

---
+++ JS_assignment/eventListiner/eventListiner.js
@@ -0,0 +1,31 @@
+
+// function singleClick() {
+//     document.getElementById("btn").innerHTML = "Thank You"
+//     document.getElementById("btn").style.backgroundColor = "red"
+//     document.getElementById("btn").style.fontSize = "34px"
+//     document.getElementById("btn").style.border = "2px solid green"
+//     document.getElementById("btn").style.padding = "10px 24px"
+//     document.getElementById("btn").style.borderRadius = "40px"
+//     document.getElementById("btn").style.color = "#fff"
+//     document.getElementById("btn").style.cursor = "pointer"
+// }
+
+// function dblClick() {
+//     document.getElementById("btn-2").innerHTML = "Thank You"
+//     document.getElementById("btn-2").style.fontSize = "34px"
+//     document.getElementById("btn-2").style.padding = "12px 24px"
+//     document.getElementById("btn-2").style.marginTop = "30px"
+//     document.getElementById("btn-2").style.border = "2px solid green"
+//     document.getElementById("btn-2").style.borderRadius = "28px"
+//     document.getElementById("btn-2").style.backgroundColor = "#000"
+//     document.getElementById("btn-2").style.color = "#fff"
+//     document.getElementById("btn-2").style.cursor = "pointer"
+// }
+
+// function imageError() {
+//     alert("The image could not be loaded.");
+// }
+
+// function onKeyPress() {
+//     alert("You pressed a key inside the input field");
+// }
