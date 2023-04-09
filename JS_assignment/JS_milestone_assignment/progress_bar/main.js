308ae12ab61f3a12b38e4e1d8fad5fcd8206588b
HasemMallick <101096018+HasemMallick@users.noreply.github.com>
2023-04-09 12:16:53 +0530

    Delete JS_assignment/JS_milestone_assignment/progress_bar directory

--- JS_assignment/JS_milestone_assignment/progress_bar/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-  let scrollTop = window.scrollY;
-  let windowHeight = window.innerHeight;
-  let documentHeight = document.body.clientHeight;
-  let progress = (scrollTop / (documentHeight - windowHeight)) * 100;
-  let progressBar = document.getElementById('progress-bar');
-  progressBar.style.width = progress + '%';
-});
