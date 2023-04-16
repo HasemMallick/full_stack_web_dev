dd541e3b99a049a80bcd8a01aef39d9e76eb8542
HasemMallick <101096018+HasemMallick@users.noreply.github.com>
2023-04-16 09:47:28 +0530

    Add files via upload

---
+++ JS_assignment/coding_chal_03.js
@@ -0,0 +1,70 @@
+// ==============================================
+
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+  
+// const result = addNumbers(5, 10);
+
+// console.log(result); 
+
+
+// ================================================
+
+// const result = multiplyNumbers(5, 10);
+
+// console.log(result);
+
+// const multiplyNumbers = function(num1, num2) {
+//     return num1 * num2;
+// };
+
+
+// ===================================================
+
+// function addNumbers(num1, num2) {
+//     console.log(myVar); 
+//     var myVar; 
+//     myVar = 10;
+//     console.log(myVar);
+//     return num1 + num2;
+// }
+  
+// const result = addNumbers(15, 12);
+// console.log(result); 
+
+
+
+// ===================================================
+
+// {
+//     console.log(myVar1); 
+//     console.log(myVar2); 
+//     console.log(myVar3); 
+  
+//     var myVar1 = 10; 
+//     let myVar2 = 20; 
+//     const myVar3 = 30; 
+  
+//     console.log(myVar1); 
+//     console.log(myVar2); 
+//     console.log(myVar3); 
+// }
+
+
+
+// ==============================================
+
+// {
+//     console.log(myVar); 
+//     let myVar; 
+
+//     console.log(myVar);
+  
+//     myVar = 10; 
+//     console.log(myVar);
+// }
+  
+  
+
+  
